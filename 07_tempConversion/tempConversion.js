3f50e82c3ee726b78a714b2737636f3537480654
Matt March <141769047+march80@users.noreply.github.com>
2024-05-14 12:08:31 -0400

    finished 5-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,6 +2,7 @@ const convertToCelsius = function (fahrenheit) {
   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
+//just adding comment holder
 const convertToFahrenheit = function (celsius) {
   return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
